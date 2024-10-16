987868d085d72bc033b6cee61d3e457df25fd774
juliaburketerreau <julia.burke-terreau@stu.ocsb.ca>
2024-10-16 14:28:29 +0000

    wrote code

--- assignment.py
+++ assignment.py
@@ -1,1 +1,8 @@
+print(""What is the diameter of the circle?")
+diameter = input()
+radius = float(diameter / 2)
+area = float((radius ** 2) * 3.14)
+circumference = float(2 * 3.14 * radius)
+print("The area is" area)
+print("The circumference is" circumference)
 
