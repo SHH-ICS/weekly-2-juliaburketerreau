b32078050504d2ecc9024ed26020873c9be7675e
juliaburketerreau <julia.burke-terreau@stu.ocsb.ca>
2024-10-16 21:54:16 +0000

    trying to get float

--- assignment.py
+++ assignment.py
@@ -1,6 +1,6 @@
 print("What is the diameter of the circle?")
-D = (input())
-radius = (D / 2)
+D = float(input())
+radius = float(D / 2)
 A = float((radius ** 2) * 3.14)
 C = float(2 * 3.14 * radius)
 print(float(A))
