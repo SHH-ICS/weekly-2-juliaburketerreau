1c64f131672ac054411f56b7db9cb8e421e95587
juliaburketerreau <julia.burke-terreau@stu.ocsb.ca>
2024-10-16 21:55:39 +0000

    added exponent

--- assignment.py
+++ assignment.py
@@ -1,7 +1,7 @@
 print("What is the diameter of the circle?")
 D = float(input())
 radius = float(D / 2)
-A = float((radius ** 2) * 3.14)
+A = float((radius ^ 2) * 3.14)
 C = float(2 * 3.14 * radius)
 print(float(A))
 print(float(C))
