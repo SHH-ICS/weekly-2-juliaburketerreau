0ec519afbd5a2e7053b8461ba374f92036a3bc67
juliaburketerreau <julia.burke-terreau@stu.ocsb.ca>
2024-10-21 15:52:48 +0000

    added floats

--- assignment.py
+++ assignment.py
@@ -1,8 +1,11 @@
+#numbers are slightly different from the tests because I only used 3.14 for pi
 print("What is the diameter of the circle?")
-D = int(input())
+D = float(input())
 radius = float(D / 2)
 A = float((radius ** 2) * 3.14)
 C = float(2 * 3.14 * radius)
-print(int(A))
-print(int(C))
+print("The area of the circle is:")
+print(float(A))
+print("The circumference of the circle is:")
+print(float(C))
 
